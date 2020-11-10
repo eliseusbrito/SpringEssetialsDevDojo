5f9fe680db007ceae01562aa4f5489ed9798cee5
eliseusbrito <eliseusbrito@gmail.com>
2020-11-10 18:37:18 -0300

    Cliente Spring com RestTemplate POST #SBE23p1

--- src/main/java/br/com/devdojo/javaClient/JavaSpringClientTest.java
+++ src/main/java/br/com/devdojo/javaClient/JavaSpringClientTest.java
@@ -4,17 +4,16 @@ import br.com.devdojo.model.PageableResponse;
 import br.com.devdojo.model.Student;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class JavaSpringClientTest {
     public static void main(String[] args) {
         RestTemplate restTemplate = new RestTemplateBuilder()
                 .rootUri("http://localhost:8080/v1/protected/students")
+                .basicAuthorization("toyo", "devdojo").build();
+        RestTemplate restTemplateAdmin = new RestTemplateBuilder()
+                .rootUri("http://localhost:8080/v1/admin/students")
                 .basicAuthorization("toyo", "devdojo").build();
         Student student = restTemplate.getForObject("/{id}", Student.class, 1);
         System.out.println(student);
@@ -30,5 +29,23 @@ public class JavaSpringClientTest {
                 new ParameterizedTypeReference<PageableResponse<Student>>() {
                 });
         System.out.println(exchange);
+        Student studentPost = new Student();
+        studentPost.setName("Jonh Wick");
+        studentPost.setEmail("jonh@pencil.com");
+        ResponseEntity<Student> exchangePost = restTemplateAdmin.exchange("/",
+                HttpMethod.POST, new HttpEntity<>(studentPost, createJSONHeader()), Student.class);
+        restTemplateAdmin.postForEntity("/",studentPost,Student.class);
+        Student studentPostForObject = restTemplateAdmin.postForObject("/", studentPost, Student.class);
+        ResponseEntity<Student> studentResponseEntity = restTemplateAdmin.postForEntity("/", studentPost, Student.class);
+        System.out.println(exchangePost);
+        System.out.println(studentPostForObject);
+        System.out.println(studentResponseEntity);
     }
+
+    private static HttpHeaders createJSONHeader(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        return headers;
+    }
+
 }
